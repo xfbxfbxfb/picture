b0f0c781e8d9312e9496023f2ea362a48a079144
xfbxfbxfb <1055153451@qq.com>
2018-12-02 11:27:28 +0800

    add photos

--- tool.py
+++ tool.py
@@ -131,7 +131,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("D:\Hexo博客图片\picture\生成的图片数据\data.json","w") as fp:
+    with open("D:\Hexo\source\photos\data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
